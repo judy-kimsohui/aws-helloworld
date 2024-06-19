42fa81245eb15dda1e1edd4bd9e88ff319240bbb
김소희 <83997634+judy-kimsohui@users.noreply.github.com>
2024-06-19 15:43:07 +0900

    Add spring file

---
+++ src/main/java/com/example/hello_world/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.example.hello_world;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
